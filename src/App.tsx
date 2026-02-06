9e7db73c544d2702c23b6c4ec0266add156bc5cb
mkmahmud <mahmudulhasanmk434@gmail.com>
2026-02-06 21:18:34 +0600

    Init with tailwind and react router

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div>   <h1 className="text-3xl font-bold underline   text-blue-600">
+            Hello Tailwind World!
+        </h1></div>
+    )
+}
